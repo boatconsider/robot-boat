ae2091c29996cc8f3c6f7c505da1b8e528b4ac5c
thapakorn odonrak <boatzarock01@gmail.com>
2023-04-26 00:43:42 +0700

    s

--- pages/index.js
+++ pages/index.js
@@ -1,132 +1,191 @@
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import { BsDot } from "react-icons/bs";
-import { HiArrowSmDown } from "react-icons/hi";
-import { HiArrowSmUp } from "react-icons/hi";
-import { BsCalendarEvent } from "react-icons/bs";
-import { useRouter } from 'next/router'
+import styles from "../styles/Home.module.css"
+import { useState } from "react";
 
 export default function Home() {
-  const router = useRouter()
-  return (
-    <div className="bg-[#F5F6F8] w-auto h-auto">
-      <div className="pl-[1rem] pt-[1rem] flex flex-row justify-between text-[#2C4860]">
-        <h1 className="text-2xl text-[#2C4860]">All my accounts</h1>
-        <div className="pr-[1rem] ">
-          <Image src="/signal (2).png" alt="" width={30} height={30} />
-        </div>
-      </div>
-      <div></div>
-      <div className="pl-[1rem] ">
-        <h1>june 10, 2018</h1>
-      </div>
-      <div className="w-full h-[15rem]  mt-[1rem] flex flex-row justify-between">
-        <div className="w-[1rem] h-[12rem] bg-[#CADEE7] rounded-md mt-[1.5rem]"></div>
-        <div className="flex justify-center items-center rounded-lg">
-          <Image src="/Visacard.png" alt="" width={330} height={600}  className="cursor-pointer" onClick={() => router.push('/Visa')}/>
-        </div>
-        <div className="w-[1rem] h-[12rem] bg-[#CADEE7] rounded-md mt-[1.5rem]"></div>
-      </div>
-      <div className="flex justify-center ">
-        <BsDot className="text-[#CADEE7] text-3xl" />{" "}
-        <BsDot className="text-[#2C4860] text-3xl" />{" "}
-        <BsDot className="text-[#CADEE7] text-3xl" />
-      </div>
-      <div className="flex justify-center items-center ">
-        <div className="w-[90%] h-[6.2rem] bg-[#fff] rounded-xl ">
-          <div className="flex flex-row justify-center items-center">
-            <div className="w-[5rem] h-[2rem] border border-slate-400 rounded-xl   mt-[1rem]">
-              <h1 className="text-center">Income</h1>
-            </div>
-            <div className="w-[5rem] h-[2rem] border  border-slate-400 rounded-xl ml-[5rem]  mt-[1rem]">
-              <h1 className="text-center">Expense</h1>
-            </div>
-          </div>
-          <div className="flex flex-row justify-center items-center">
-            <div className="pt-[1rem] text-xl text-[#27F487] flex flex-row ">
-              <HiArrowSmDown className="text-4xl text-[#27F487]" />
-              <h1> $9,302.00</h1>
-            </div>
-            <div className="pt-[1rem] text-xl text-[#F42727] flex flex-row pl-[3rem]">
-              <HiArrowSmUp className="text-4xl text-[#F42727]" />
-              <h1> $2,790.00</h1>
-            </div>
-          </div>
-        </div>
-      </div>
+  const [inputs, setInputs] = useState({ input: "" });
+  const [error, setError] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [showTable, setShowTable] = useState(false);
+  const [position, setPosition] = useState({ x: 4, y: 4 });
+  const [direction, setDirection] = useState("up");
 
-      <div className="flex flex-row justify-between text-[#2C4860] pl-[1rem] text-xl mt-[1rem]">
-        <h1>Detail of movements</h1>
-        <div className="text-2xl text-[#2C4860] pr-[1rem]">
-          <BsCalendarEvent />
-        </div>
-      </div>
-      <div className="flex justify-center items-center pt-[1rem] flex-col">
-        <div className="w-[90%] h-[4.5rem] bg-[#fff] rounded-xl ">
-          <div className="flex flex-row items-center justify-between">
-            <div className="pl-[1rem] pt-[0.7rem] flex flex-row  items-center">
-              <Image src="/dish.png" alt="" width={40} height={40} />
-              <div className="flex flex-col pl-[1rem]">
-                <h1 className="font-bold text-md ">Restaurant Manhattan</h1>
-                <h5>june 10, 2018</h5>
-              </div>
-            </div>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const regex = /^[WLR]*$/;
 
-            <div className="flex justify-end pr-[1rem] pt-[1rem] items-center">
-              <h1 className="text-xl text-[#F42727]">$170 </h1>
-              <HiArrowSmUp className="text-4xl text-[#F42727]" />
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] h-[4.5rem] bg-[#fff] rounded-xl mt-[0.5rem]">
-          <div className="flex flex-row items-center justify-between">
-            <div className="pl-[1rem] pt-[0.7rem] flex flex-row ">
-              <Image src="/save-money.png" alt="" width={40} height={40} />
-              <div className="flex flex-col pl-[1rem]">
-                <h1 className="font-bold text-md">Salary Deposit</h1>
-                <h5>june 1, 2018</h5>
-              </div>
-              </div>
-              <div className="flex justify-center pr-[1rem] pt-[1rem]  items-center">
-                <h1 className="text-xl text-[#27F487]">$1200 </h1>
-                <HiArrowSmDown className="text-4xl text-[#27F487]" />
-              </div>
-          
-          </div>
-        </div>
-        <div className="w-[90%] h-[4.5rem] bg-[#fff] rounded-xl mt-[0.5rem]">
-          <div className="flex flex-row items-center justify-between">
-            <div className="pl-[1rem] pt-[0.7rem] flex flex-row ">
-              <Image src="/paper-bags.png" alt="" width={40} height={40} />
-              <div className="flex flex-col pl-[1rem]">
-                <h1 className="font-bold text-md">Central Market</h1>
-                <h5>may 28, 2018</h5>
-              </div>
-              </div>
-              <div className="flex justify-end pr-[1rem] items-center">
-                <h1 className="text-xl text-[#F42727]">$50 </h1>
-                <HiArrowSmUp className="text-4xl text-[#F42727]" />
-              </div>
+    if (regex.test(value)) {
+      setInputs({ ...inputs, [name]: value });
+      setError(null);
+      setIsDisabled(value.trim() === "");
+    } else {
+      setError("สามารถพิมพ์ได้แค่ W, L, R");
+      setIsDisabled(true);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowTable(true);
+    handleInput(inputs.input);
+    setInputs({ input: "" });
+  };
+
+  const handleInput = (input) => {
+    let stepsToWalk = 0;
+  
+    const handleAction = (action) => {
+      switch (action) {
+        case "W":
+          handleWalk(stepsToWalk);
+          stepsToWalk = 0;
+          break;
+        case "L":
+          handleTurnLeft();
+          break;
+        case "R":
+          handleTurnRight();
+          break;
+        default:
+          break;
+      }
+    };
+  
+    for (let i = 0; i < input.length; i++) {
+      const action = input[i];
+      if (action === "W") {
+        stepsToWalk++;
+      } else if (action !== "") { // Skip over empty values
+        handleAction(action);
+      }
+    }
+    handleWalk(stepsToWalk);
+  };
+  
+  
+  const handleWalk = (steps) => {
+    setVisitedCells({ ...visitedCells, [`${position.x},${position.y}`]: true });
+    
+    switch (direction) {
+      case "up":
+        setPosition({ ...position, y: position.y - steps });
+        break;
+      case "down":
+        setPosition({ ...position, y: position.y + steps });
+        break;
+      case "left":
+        setPosition({ ...position, x: position.x - steps });
+        break;
+      case "right":
+        setPosition({ ...position, x: position.x + steps });
+        break;
+      default:
+        break;
         
-          </div>
-        </div>
-        <div className="w-[90%] h-[4.5rem] bg-[#fff] rounded-xl  mt-[0.5rem]">
-          <div className="flex flex-row items-center justify-between">
-            <div className="pl-[1rem] pt-[0.7rem] flex flex-row ">
-              <Image src="/money.png" alt="" width={40} height={40} />
-              <div className="flex flex-col pl-[1rem]">
-                <h1 className="font-bold text-md">Salary Deposit</h1>
-                <h5>may 1, 2018</h5>
-              </div>
-              </div>
-              <div className="flex justify-end pr-[1rem] items-center">
-                <h1 className="text-xl text-[#27F487]">$1200 </h1>
-                <HiArrowSmDown className="text-4xl text-[#27F487]" />
-              </div>
+    }
+  };
+  const handleTurnLeft = () => {
+    switch (direction) {
+      case "up":
+        setDirection("left");
+        break;
+      case "down":
+        setDirection("right");
+        break;
+      case "left":
+        setDirection("down");
+        break;
+      case "right":
+        setDirection("up");
+        break;
+      default:
+        break;
+    }
+  };
+  
+  const handleTurnRight = () => {
+    switch (direction) {
+      case "up":
+        setDirection("right");
+        break;
+      case "down":
+        setDirection("left");
+        break;
+      case "left":
+        setDirection("up");
+        break;
+      case "right":
+        setDirection("down");
+        break;
+      default:
+        break;
+    }
+  };
+  const [visitedCells, setVisitedCells] = useState({});
+
+  const renderTable = () => {
+    return (
+      <table className="mt-4 border-collapse border border-gray-400 " id={styles.id}>
+        <tbody>
+          {Array.from(Array(9)).map((_, i) => (
+            <tr key={i}>
+              {Array.from(Array(9)).map((_, j) => {
+                const isCurrentCell = i === position.y && j === position.x;
+                const isVisitedCell = visitedCells[`${j},${i}`];
+                const backgroundColor = isCurrentCell ? "green" : isVisitedCell ? "gray" : "white";
+                return (
+                  <td
+                    key={`${i}-${j}`}
+                    className="border border-gray-400 text-center h-8 w-8"
+                    style={{ backgroundColor }}
+                  >
+                    {""}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div className="pl-[2rem] pt-[1.5rem]">
       
-          </div>
-        </div>
+     
+      <div className="max-[768px]:flex flex-col justify-center items-center pt-[1rem]">
+      
+      <form onSubmit={handleSubmit}>
+        <h1>Robot Walk</h1>
+      <h1>คำสั่ง Robot Walk</h1>
+        <input
+          type="text"
+          name="input"
+          value={inputs.input}
+          onChange={handleChange}
+          className="w-[10rem] h-[2rem] px-2 text-black bg-gray-200 rounded-md outline-none"
+        />
+
+        <button
+          type="submit"
+          disabled={isDisabled}
+          className={`w-[5rem] ml-[1rem] h-[2rem] px-2 rounded-md ${
+            isDisabled
+              ? "bg-gray-400 outline-none text-white cursor-default"
+              : "bg-slate-300 text-black hover:bg-slate-400 cursor-pointer"
+          }`}
+        >
+          Submit
+        </button>
+    
+        {error && <div className="text-red-500 mt-1">{error}</div>}
+      </form>
+       <div >
+      {showTable && renderTable()} {}
       </div>
+      </div>
+     
+      
     </div>
   );
 }
